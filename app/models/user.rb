28e3844a6de8ee3b368bfd401652045782b10cd0
EC2 Default User <ec2-user@ip-172-31-12-196.ap-northeast-1.compute.internal>
2022-04-20 10:14:14 +0000

    user 詳細編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < ApplicationRecord
   
     has_many :camps, dependent: :destroy
     has_one_attached :profile_image
+    
+     enum area:{
+     北海道地方:0,
+     東北地方:1,関東地方:2,中部地方:3,近畿地方:4,中国・四国地方:5,九州地方:6
+   }
+     enum level:{
+     超初心者:0,
+     初心者:1,中級者:2,中上級者:3,上級者:4,超上級者:5,達人:6
+   }
   
 
 end
