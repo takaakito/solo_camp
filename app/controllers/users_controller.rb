b1784c59febff39ec2f5cf55911eefff3eebda0c
EC2 Default User <ec2-user@ip-172-31-12-196.ap-northeast-1.compute.internal>
2022-06-09 07:27:33 +0000

    index layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @camps = @user.camps
     @foods = @user.foods
+    @plays = @user.plays
     @user_favorites = @user.favorites
     @user_food_favorites = @user.food_favorites
+    @user_play_favorites = @user.play_favorites
     @camp_all_ranks = Camp.find(Favorite.group(:camp_id).order('count(camp_id) desc').limit(3).pluck(:camp_id))
     @camp_my_ranks = @camp_all_ranks.select{ |food| food.user_id == current_user.id }
     @food_all_ranks = Food.find(FoodFavorite.group(:food_id).order('count(food_id) desc').limit(3).pluck(:food_id))
     @food_my_ranks = @food_all_ranks.select{ |food| food.user_id == current_user.id }
+    @play_all_ranks = Play.find(PlayFavorite.group(:play_id).order('count(play_id) desc').limit(3).pluck(:play_id))
+    @play_my_ranks = @play_all_ranks.select{ |play| play.user_id == current_user.id }
   end
   
   def update
