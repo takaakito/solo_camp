c6c596c639f93916ababcf14812b3a6424b6036d
EC2 Default User <ec2-user@ip-172-31-12-196.ap-northeast-1.compute.internal>
2022-06-20 15:39:54 +0000

    main

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,17 @@
 class HomesController < ApplicationController
   def top
+    @camps = Camp.order(created_at: :desc).limit(3)
+    @foods = Food.order(created_at: :desc).limit(3)
+    @plays = Play.order(created_at: :desc).limit(3)
+  end
+  
+  def guest_sign_in
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+  end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  
+  post '/homes/guest_sign_in', to: 'homes#guest_sign_in'
+  
   resources :users, only: [:show, :edit, :update]
   
   resources :foods, only: [:new, :create, :index, :show, :destroy]do
